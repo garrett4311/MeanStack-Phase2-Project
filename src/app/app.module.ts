b0ddffbf472d87d85f9a93242929960ada5ba3ea
Garrett Morris <garrettmorris@Garretts-MBP.home>
2020-11-06 16:49:32 -0600

    initial commit, finished project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { QuizComponent } from './quiz/quiz.component';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    QuizComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [QuizComponent]
 })
 export class AppModule { }
---
+++ src/app/model.quiz.ts
@@ -0,0 +1,4 @@
+export class Quiz {
+    constructor(public question: string, public a:string, public b:string,
+        public c:string, public d:string, public answer:string) {}
+}
